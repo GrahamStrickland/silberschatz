bf28c053edfe254f87f82c2751ab2d51e5e40ffe
Graham Strickland <grahamhstrickland@gmail.com>
2023-03-20 18:35:59 +0200

    Add code for ch03

---
+++ ch03/3_3/unix_fork/build/CMakeFiles/unix_fork.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/unix_fork.dir/main.c.o"
+  "CMakeFiles/unix_fork.dir/main.c.o.d"
+  "unix_fork"
+  "unix_fork.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/unix_fork.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ch03/3_3/unix_fork/build/CMakeFiles/unix_fork.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for unix_fork.
+# This may be replaced when dependencies are built.
